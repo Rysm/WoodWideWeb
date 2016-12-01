1b80560f01722cd0ff43158efc6ba334b963b250
sonhuynh@ucsc.edu <number1>
2016-12-01 09:57:18 -0800

    Minor working with color shift

--- myceliaNet/Assets/UIScript/LandColorChange.cs
+++ myceliaNet/Assets/UIScript/LandColorChange.cs
@@ -3,22 +3,34 @@ using System.Collections;
 
 public class LandColorChange : MonoBehaviour {
 
-    public float color = 0f;
-    public UnityEngine.UI.Image image;
+    float slider = 0f, t = 0f, duration;
+    UnityEngine.UI.Image image;
+    Color color1, color2;
+
+    Color temp1 = Color.red, temp2 = Color.blue;
 
 	// Use this for initialization
 	void Start () {
         image = GetComponent<UnityEngine.UI.Image>();
+        image.color = Color.red;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (color == 10f) {
-            image.color = Color.red;
-        }
+        Color color = Color.Lerp(temp1, temp2, t);
+        t += Time.deltaTime * 2;
+        image.color = color;
     }
 
-    public void AdjustColor (float newColor) {
-        color = newColor;
+    public void AdjustColor (float newSlider) {
+        slider = newSlider;
+        color1 = image.color;
+        if (slider == 0) {
+            color2 = new Color(0f / 255f, 139f / 255f, 139f / 255f);
+        } else if (slider == 1) {
+            color2 = new Color(34f / 255f, 139f / 255f, 34f / 255f);
+        } else if (slider == 2) {
+            color2 = new Color(255f / 255f, 248f / 255f, 220f / 255f);
+        }
     }
 }
