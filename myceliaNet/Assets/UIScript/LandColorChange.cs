788035228a7bb567c9c2047f14f023ef7e22bad2
sonhuynh@ucsc.edu <number1>
2016-12-01 15:49:39 -0800

    Color change works

--- myceliaNet/Assets/UIScript/LandColorChange.cs
+++ myceliaNet/Assets/UIScript/LandColorChange.cs
@@ -3,34 +3,35 @@ using System.Collections;
 
 public class LandColorChange : MonoBehaviour {
 
-    float slider = 0f, t = 0f, duration;
+    public float slider = 0f;
     UnityEngine.UI.Image image;
-    Color color1, color2;
-
-    Color temp1 = Color.red, temp2 = Color.blue;
+    private Color newColour;
 
 	// Use this for initialization
 	void Start () {
         image = GetComponent<UnityEngine.UI.Image>();
-        image.color = Color.red;
+        //image.color = Color.red;
+        newColour = image.color;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Color color = Color.Lerp(temp1, temp2, t);
-        t += Time.deltaTime * 2;
-        image.color = color;
+        //image.color = Color.Lerp(color1, color2, Time.deltaTime);
+        //t += Time.deltaTime * 2;
+        ChangeColor();//image.color = color;
+    }
+
+    void ChangeColor () {
+        if (slider == 0) //powder blue
+            newColour = new Color(176f / 255f, 224f / 255f, 230f / 255f);
+        else if (slider == 1) //forest green
+            newColour = new Color(34f / 255f, 139f / 255f, 34f / 255f);
+        else if (slider == 2) //corn silk
+            newColour = new Color(255f / 255f, 248f / 255f, 220f / 255f);
+        image.color = Color.Lerp(image.color, newColour, Time.deltaTime * 2);
     }
 
     public void AdjustColor (float newSlider) {
         slider = newSlider;
-        color1 = image.color;
-        if (slider == 0) {
-            color2 = new Color(0f / 255f, 139f / 255f, 139f / 255f);
-        } else if (slider == 1) {
-            color2 = new Color(34f / 255f, 139f / 255f, 34f / 255f);
-        } else if (slider == 2) {
-            color2 = new Color(255f / 255f, 248f / 255f, 220f / 255f);
-        }
     }
 }
